f515fb35e354e6afeb7f448d101a52012c635fa5
Sakhare Divya <sakhredivya@gmail.com>
2025-05-13 08:47:10 -0700

    Add variable.js with account details and logging functionality

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountId = 123233
+let accountEmail = "sakharedivya@gmail.com"
+var accountPassword ="12345"
+accountCity ="pune"
+
+accountEmail ="divya@,com"
+accountPassword ="2344566"
+accountCity = "mumbai"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
