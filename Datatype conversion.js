368ac46cecc1c5c857bce5113d4e0733df6ca672
Sakhare Divya <sakhredivya@gmail.com>
2025-05-13 10:05:38 -0700

    Clean up comments in Datatype conversion.js for clarity and consistency

--- Datatype conversion.js
+++ Datatype conversion.js
@@ -7,3 +7,17 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0 
+
+// let isLoggeIn = "Divya"
+
+// let booleanIsLoggedIn = boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0=> false
+//" "=> false
+// "Divya" => true
