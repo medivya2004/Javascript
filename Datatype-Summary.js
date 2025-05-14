7624de69d3e3d6c76d52777374ac82437760dc98
Sakhare Divya <sakhredivya@gmail.com>
2025-05-14 01:21:20 -0700

    Add Datatype-Summary.js with initial primitive and reference type examples

---
+++ Datatype-Summary.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// 7 Types : String
+             //Number
+             //Boolean
+             //null
+             //undefined
+             //Symbol
+             //BigIn
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn =false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+//const bigNumber = 345654357665435754n
+ 
+//Reference (Non primitive)             
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj ={
+    name:"Divya",
+    age:22,
+}
+
+const myFunction = function()
+{
+    console.log("Hello World");
+    
+}
+
+console.log(typeof anotherId);
+console.log(typeof myFunction);
+
