521707f6f54b65014a0899c660beb8762b598ec2
Sakhare Divya <sakhredivya@gmail.com>
2025-05-25 02:29:22 -0700

    Refactor addTwoNumbers function to return result and log username in loginUserMessage

---
+++ Function & Parameter.js
@@ -0,0 +1,39 @@
+function saymyName()
+{
+    console.log("D");
+    console.log("I");
+    console.log("V");
+    console.log("Y");
+    console.log("A");
+    
+}
+
+//saymyName()
+
+function addTwoNumbers(number1, number2)
+{
+    console.log(number1 + number2);
+}
+addTwoNumbers(3, 4)
+
+
+function addTwoNumbers(number1, number2)
+{
+    let result = number1 + number2
+    console.log("Divya");
+    return result
+}
+
+const result = addTwoNumbers(3, 5)
+console.log("Result: ",result);
+
+function loginUserMessage(username)
+{
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Divya"))
+
+console.log(loginUserMessage())
+
+
